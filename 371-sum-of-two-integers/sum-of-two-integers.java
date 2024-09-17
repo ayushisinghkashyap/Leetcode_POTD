97b6cb84b0304b170a1b9d3bfa40a0f88a104df7
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-09-17 22:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (18.46%) - LeetSync

---
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int getSum(int a, int b) {
+        return a+b;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(getSum(a,b));
+    }
+}
